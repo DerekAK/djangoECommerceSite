fccb041a643ad495bc412c52b0b64e46b2014c70
Derek Araki-Kurdyla <djak001@bucknell.edu>
2024-07-09 09:28:13 -0400

    installed/connected mysql

--- djangoTutorial/settings.py
+++ djangoTutorial/settings.py
@@ -87,8 +87,11 @@ WSGI_APPLICATION = 'djangoTutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'store',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'GrumpyScroob66!'
     }
 }
 
